324dd1329c29e76fe8ae11b1aa45b0f28a3bf649
Szymon440 <szymonss@gmail.com>
2024-03-07 17:29:33 +0100

    feat: task-module

--- src/core/modules/task/models/task.py
+++ src/core/modules/task/models/task.py
@@ -5,6 +5,6 @@ from time_sheet.src.core.modules.common.models.base_model import MongoDBModel
 
 class Task(MongoDBModel):
     name: str = Field(..., min_length=5, max_length=50)
-    start: datetime = None
-    end: datetime = None
+    start: datetime = datetime.now()
+    end: datetime = datetime.now()
     additional_information: str = Field(..., min_length=5, max_length=100)
